55ca6e7f00103b5e26a2acf63d9cb919125ce439
Andy Striph <ajstriph@me.com>
2017-10-04 20:10:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,6 +37,6 @@ function destructivelyRemoveElementFromEndOfArray (array){
   return array;
 }
 function removeElementFromEndOfArray (array){
-  array.slice(0,3);
+  array.slice(0,2);
   return array;
 }
