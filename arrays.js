2066c0b0f2a662fa116b295915a61429a28d298a
Andy Striph <ajstriph@me.com>
2017-10-04 19:15:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,8 +8,7 @@ function addElementToBeginningOfArray (array, element){
 
 function destructivelyAddElementToBeginningOfArray (array, element){
   array = [1]
-  array.unshift("foo");
-  return array;
+  return array.unshift("foo");
 }
 function addElementToEndOfArray (array, element){
   addElementToEndOfArray = [1];
