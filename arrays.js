74900f45c6eb6a70415c3672e91f2e923c24222a
Andy Striph <ajstriph@me.com>
2017-10-04 17:54:15 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+function addElementToBeginningOfArray (array, element){
+  addElementToBeginningOfArray.unshift("element");
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray (addElementToBeginningOfArray, element){
+  addElementToBeginningOfArray.unshift("element");
+  return array;
+}
