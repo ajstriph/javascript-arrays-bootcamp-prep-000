95a62993bb0ad279000dff0bd306cd1013767a55
Andy Striph <ajstriph@me.com>
2017-10-04 18:11:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,22 +2,23 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray (array, element){
   const addElementToBeginningOfArray = [1];
-  const addElementToNewArray = ['foo',...addElementToBeginningOfArray];
+  const addElementToNewArray = ["foo",...addElementToBeginningOfArray];
   return array;
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element){
   addElementToBeginningOfArray = [1];
-  addElementToBeginningOfArray.unshift('foo');
+  addElementToBeginningOfArray.unshift("foo");
   return array;
 }
 function addElementToEndOfArray (array, element){
   const addElementToEndOfArray = [1];
-  const addElementtoEndofNewArray = [...addElementToEndOfArray, 'foo'];
+  const addElementtoEndofNewArray = [...addElementToEndOfArray, "foo"];
   return array;
 }
 function destructivelyAddElementToEndOfArray (array, element){
   destructivelyAddElementToEndOfArray = [1];
-  destructivelyAddElementToEndOfArray.push('foo');
+  destructivelyAddElementToEndOfArray.push("foo");
   return array;
 }
+
